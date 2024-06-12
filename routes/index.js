c92f977673e83c3d605e2130735034b0b18a387e
sudorootec <bunnybatru@gmail.com>
2024-06-13 01:59:39 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,126 +1,101 @@
 const express = require('express');
 const bcrypt = require("bcrypt");
 const router = express.Router();
-
-const axios = require('axios');
 const User = require("../models/user");
 
 /* GET home page. */
-router.get('/', ensureAuthenticated, (req, res) => {
-  //console.log("Browser session"); console.log(req.session);
-  res.render('index', {
-    title: 'Donut Clicker',
-    user: req.user
-  });
+router.get('/', ensureAuthenticated, async (req, res, next) => {
+  try {
+    res.render('index', {
+      title: 'Donut Clicker',
+      user: req.user
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  const id = req.body.id;
-  const username = req.body.username;
-  const email = req.body.email;
-  let password_test = null;
+router.post('/', async (req, res, next) => {
+  try {
+    const id = req.body.id;
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
 
-  User.findById(req.body.id, function (err, user) {
-    if (err)
-      throw err;
-    if (user.username !== username || user.email !== email || req.body.password.length !== 0) {
+    const user = await User.findById(id);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    if (user.username !== username || user.email !== email || password.length !== 0) {
       if (user.username !== username) {
-        req
-          .checkBody("username", "Username is required")
-          .notEmpty();
-        req
-          .checkBody("username", "Username Already Exist")
-          .isUniqueUsername();
+        req.checkBody("username", "Username is required").notEmpty();
+        req.checkBody("username", "Username Already Exist").isUniqueUsername();
       }
       if (user.email !== email) {
-        req
-          .checkBody("email", "Email is required")
-          .notEmpty();
-        req
-          .checkBody("email", "Email is not valid")
-          .isEmail();
-        req
-          .checkBody("email", "Email already exist")
-          .isUniqueEmail();
+        req.checkBody("email", "Email is required").notEmpty();
+        req.checkBody("email", "Email is not valid").isEmail();
+        req.checkBody("email", "Email already exists").isUniqueEmail();
       }
-      const result = req.getValidationResult().then(err => {
-        const errors = err.array();
-        console.log(errors);
-        if (!err.isEmpty()) {
-          res.render("index", {
-            errors: errors
-          });
-        } else {
-          bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(req.body.password, salt, (err, hash) => {
-              user.username = username;
-              user.email = email;
-              if (req.body.password.length !== 0) {
-                user.password = hash;
-              }
-              user
-                .save(function (err) {
-                  if (err)
-                    throw err;
-                  console.log("user saved");
-                  res.redirect(req.originalUrl);
-                });
-            });
-          });
+      const result = await req.getValidationResult();
+      if (!result.isEmpty()) {
+        const errors = result.array();
+        res.render("index", { errors });
+      } else {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+        user.username = username;
+        user.email = email;
+        if (password.length !== 0) {
+          user.password = hash;
         }
-      });
+        await user.save();
+        res.redirect(req.originalUrl);
+      }
     }
-  });
-});
-router.get('/backup', (req, res) => {
-
-  if ('id' in req.query) {
-    console.log(req.query.id + ' : Retrieve Backup...');
-    User.getUserById(req.query.id, (err, user) => {
-      if (err)
-        throw err;
-
-      res.send({
-        backup: user.backup
-      });
-      console.log(req.query.id + ' : Backup loaded');
-    });
-  } else {
-    console.log('Undefined user');
-    console.log('No Backup');
-    res.send({
-      backup: null
-    });
+  } catch (err) {
+    next(err);
   }
 });
 
-router.put('/save', (req, res) => {
-  if ('id' in req.body) {
-    console.log(req.body.id + ' : Saving...');
-    // console.log('Game info are :'); console.log(JSON.stringify(req.body));
-    // console.log(req.user);
-    User.getUserById(req.body.id, (err, user) => {
-      if (err)
-        throw err;
+router.get('/backup', async (req, res, next) => {
+  try {
+    const userId = req.query.id;
+    if (!userId) {
+      throw new Error('Undefined user');
+    }
 
-      user.backup = req.body.backup;
+    const user = await User.getUserById(userId);
+    if (!user) {
+      throw new Error('User not found');
+    }
 
-      user.save(function (err) {
-        if (err)
-          throw err;
-        console.log(req.body.id + ' : Game Saved !');
-        res.send({
-          saved: true
-        });
-      });
-    });
-  } else {
-    console.log('Undefined user');
-    res.send({
-      saved: false
-    });
+    res.json({ backup: user.backup });
+  } catch (err) {
+    next(err);
   }
 });
+
+router.put('/save', async (req, res, next) => {
+  try {
+    const userId = req.body.id;
+    if (!userId) {
+      throw new Error('Undefined user');
+    }
+
+    const user = await User.getUserById(userId);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    user.backup = req.body.backup;
+    await user.save();
+    res.json({ saved: true });
+  } catch (err) {
+    next(err);
+  }
+});
+
 router.get('/forgot_password', (req, res) => {
   res.render('forgot', {
     title: 'Forgot password - Donut Clicker',
@@ -128,28 +103,25 @@ router.get('/forgot_password', (req, res) => {
   });
 });
 
-router.post('/forgot_password', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const password_c = req.body.password_c;
-  if (email.length > 0 && password.length > 0 && password_c.length > 0 && password === password_c) {
-    User.getUserByEmail(req.body.email, (err, user) => {
-      if (!err && user) {
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(password, salt, (err, hash) => {
-            user.password = hash;
-            user.save(function (err) {
-              if (err)
-                throw err;
-              console.log(req.body.email + ' : Reset done !');
-            });
-          });
-        });
+router.post('/forgot_password', async (req, res, next) => {
+  try {
+    const { email, password, password_c } = req.body;
+    if (email.length > 0 && password.length > 0 && password === password_c) {
+      const user = await User.getUserByEmail(email);
+      if (!user) {
+        throw new Error('User not found');
       }
-    });
-    res.redirect('/users/login');
-  } else {
-    res.redirect('/forgot_password');
+
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, salt);
+      user.password = hash;
+      await user.save();
+      res.redirect('/users/login');
+    } else {
+      res.redirect('/forgot_password');
+    }
+  } catch (err) {
+    next(err);
   }
 });
 
@@ -160,4 +132,5 @@ function ensureAuthenticated(req, res, next) {
     res.redirect('/users/login');
   }
 }
+
 module.exports = router;
